ccfa9604076a2c8befe2b8103a181e1fdefaf523
JHLee <leejunhee1219@snu.ac.kr>
2023-07-15 20:25:15 +0900

    asdf

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+// Service Worker 등록
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('service-worker.js')
+      .then(function(registration) {
+        console.log('Service Worker 등록 성공:', registration.scope);
+      })
+      .catch(function(error) {
+        console.log('Service Worker 등록 실패:', error);
+      });
+  });
+}
+
+
+
 let menuItems = document.querySelectorAll('.menu-item');
 const arrowDown = document.querySelector('.arrow-down');
 const arrowUp = document.querySelector('.arrow-up');
